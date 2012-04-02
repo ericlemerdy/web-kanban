25375df2ba56356a9bc7ea72e9e0b1de8e40a2cf
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-04-02 03:54:36 +0200

    Renaming and a surprise when adding a story :-) #5

--- src/main/webapp/js/web-kanban.js
+++ src/main/webapp/js/web-kanban.js
@@ -28,8 +28,8 @@ var init_board = function (stories) {
     return board;
 };
 
-var create_box = function (state, board, i) {
-    var story_element = $('<li class="box box_' + state + '">' + board[state][i][1] + ' ' + board[state][i][2] + '</li>');
+var create_story = function (state, board, i) {
+    var story_element = $('<li class="story story_' + state + '">' + board[state][i][1] + ' ' + board[state][i][2] + '</li>');
     story_element.data("story", board[state][i]);
     return story_element;
 };
@@ -38,15 +38,15 @@ var create_list = function (board, state) {
     var list = $('<ul class="state" id="' + state + '"></ul>');
     if (board[state]) {
         for (var i = 0, len = board[state].length; i < len; i++) {
-            var story_element = create_box(state, board, i);
+            var story_element = create_story(state, board, i);
             list.append(story_element);
         }
     }
     return list;
 };
 
-var create_column = function (board, state, headline) {
-    var state_column = $('<section class="column"></div>');
+var create_state_column = function (board, state, headline) {
+    var state_column = $('<section class="state-column"></div>');
     state_column.append($('<header class="headline">' + headline + '</header>'));
     state_column.append(create_list(board, state));
     state_column.data("state", state);
@@ -62,10 +62,11 @@ var create_board = function (app_data) {
     var button = $('<button id="new-story">Add story</button>');
     button.button();
     button.click(function () {
-        var new_box = create_box("TODO", {"TODO":[
+        var new_box = create_story("TODO", {"TODO":[
             ["", "0", $('#new-story-text').val()]
         ]}, 0);
         $('#TODO').append(new_box);
+        new_box.show("drop");
     });
     aside.append(button);
 
@@ -73,7 +74,7 @@ var create_board = function (app_data) {
     for (j = 0; j < app_data.states_order.length; j++) {
         var state = app_data.states_order[j];
         ids += ((ids.length == 0) ? '' : ', ') + '#' + state;
-        var state_column = create_column(app_data.board, state, app_data.states[state]);
+        var state_column = create_state_column(app_data.board, state, app_data.states[state]);
         table.append(state_column);
     }
     $(function () {
